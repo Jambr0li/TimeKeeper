5cfb814efc912323c70f11d8bf68bc9c645bc552
Jambr0li <jasonkovalenko@gmail.com>
2023-10-26 17:14:55 -0700

    added chart.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,6 +13,23 @@ class Activity {
 }
 const user = new User("Jason");
 userName.textContent = user.name;
+
+const data = {
+  labels: ["CS360", "CS317", "STATS360", "CS425"],
+  datasets: [
+    {
+      data: [3, 2, 2, 3], // Replace with your actual data values
+      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#EAF6FF"], // Colors for each section
+    },
+  ],
+};
+
+const ctx = document.getElementById("myPieChart").getContext("2d");
+const myPieChart = new Chart(ctx, {
+  type: "pie",
+  data: data,
+});
+
 // git add .
 // git commit -m "message"
 // git push origin master
